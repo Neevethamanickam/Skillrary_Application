94032feacf528633a0e3f26f07856d60b511445b
Neevetha Manickam <Neevetha Manickam@Neevetha>
2021-10-17 20:37:36 +0530

    Initial Commit

---
+++ src/main/java/pom/Addtocartpage.java
@@ -0,0 +1,29 @@
+package pom;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Addtocartpage {
+	
+	@FindBy(id="add")
+	private WebElement addbtn;
+	
+	@FindBy(xpath="//button[text()='Add to Cart")
+	private WebElement addtocart;
+	
+	public Addtocartpage(WebDriver driver)
+	{
+		PageFactory.initElements(driver, this);
+	}
+	
+	public WebElement getAddbtn()
+	{
+		return addbtn;
+	}
+    public void addtocartbtn()
+    {
+    	addtocart.click();
+    }
+}
